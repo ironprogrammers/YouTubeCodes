34f7f4fc2b1f78b97b9b6c48d8b1d70b32865faf
Samriddhi Agarwal <samriddhiagarwal12@gmail.com>
2020-10-01 14:04:54 +0530

    Python Basics of Strings

---
+++ Python Basics/string.py
@@ -0,0 +1,17 @@
+"""
+Strings are immutable
+"""
+a="hello"
+a[0]="S" #invalid 
+
+"""
+3 ways to create strings
+"""
+string = "Hello"
+string = 'Hello'
+string = """ Hello """
+
+"""
+String length calculation
+"""
+print(len(string))
